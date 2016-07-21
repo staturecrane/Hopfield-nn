1270ed04c9e3767efe614800070abd2f7cf2771a
Richard Herbert <richard.alan.herbert@gmail.com>
2016-07-20 22:41:19 -0800

    update constructor syntax

--- Project1/Hopfield.cpp
+++ Project1/Hopfield.cpp
@@ -11,29 +11,29 @@ VectorXd vec_to_vec(std::vector<int>, int);
 int sign(int);
 
 int main() {
-	MatrixXd weights = { createWeights(5,5) };
-	MatrixXd new_weights = { train(weights, { -1, 1, 1, -1, 1}, 5) };
-	MatrixXd d_new_weights = { train(new_weights,{ 1, -1, 1, -1, 1 }, 5) };
+	MatrixXd weights { createWeights(5,5) };
+	MatrixXd new_weights { train(weights, { -1, 1, 1, -1, 1}, 5) };
+	MatrixXd d_new_weights { train(new_weights,{ 1, -1, 1, -1, 1 }, 5) };
 	std::cout << d_new_weights << std::endl;
-	int energy = update(d_new_weights, 3, { 1, 1, 1, 1, 1});
+	int energy { update(d_new_weights, 3, { 1, 1, 1, 1, 1}) };
 	std::cout << "Energy of third node is " << energy << std::endl;
 	return 0;
 }
 
 MatrixXd createWeights(int i, int j) {
-	MatrixXd weights = { MatrixXd::Zero(i, j) };
+	MatrixXd weights { MatrixXd::Zero(i, j) };
 	return weights; 
 }
 
 MatrixXd train(MatrixXd oldWeights, std::vector<int> pattern, int i) {
-	VectorXd v_pattern = vec_to_vec(pattern, i);
-	MatrixXd newWeights = v_pattern * v_pattern.transpose() - MatrixXd::Identity(i,i);
+	VectorXd v_pattern { vec_to_vec(pattern, i) };
+	MatrixXd newWeights { v_pattern * v_pattern.transpose() - MatrixXd::Identity(i,i) };
 	return oldWeights + newWeights;
 }
 
 int update(MatrixXd weights, int i, std::vector<int> pattern) {
-	VectorXd v_pattern{ vec_to_vec(pattern, weights.col(0).size()) };
-	int energy = weights.col(i - 1).dot(v_pattern.transpose());
+	VectorXd v_pattern { vec_to_vec(pattern, weights.col(0).size()) };
+	int energy = weights.col(i - 1).dot(v_pattern.transpose()) ;
 	return energy;
 }
 
