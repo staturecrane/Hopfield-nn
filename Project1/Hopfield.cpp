9e12acbd95346f54e12df1d4b42ba9e933ad40b2
Richard Herbert <richard.alan.herbert@gmail.com>
2016-07-21 08:47:00 -0700

    add header, add pattern recovery

--- Project1/Hopfield.cpp
+++ Project1/Hopfield.cpp
@@ -1,52 +1,11 @@
-#include <iostream>
-#include <Eigen/Dense> 
-#include <vector>
-
-using namespace Eigen; 
-
-MatrixXd createWeights(int, int);
-MatrixXd train(MatrixXd, std::vector<int>, int);
-int getEnergy(MatrixXd, int, std::vector<int>);
-VectorXd vec_to_vec(std::vector<int>, int);
-int sign(int);
+#include "Hopfield.h"
 
 int main() {
 	MatrixXd weights { createWeights(5,5) };
 	MatrixXd new_weights { train(weights, { -1, 1, 1, -1, 1}, 5) };
 	MatrixXd d_new_weights { train(new_weights,{ 1, -1, 1, -1, 1 }, 5) };
 	std::cout << d_new_weights << std::endl;
-	int energy { getEnergy(d_new_weights, 3, { 1, 1, 1, 1, 1}) };
-	std::cout << "Energy of third node is " << energy << std::endl;
+	VectorXd recovered = recover(d_new_weights, { 1, 1, 1, 1, 1 });
+	std::cout << "Recovered pattern is " << recovered << std::endl;
 	return 0;
-}
-
-MatrixXd createWeights(int i, int j) {
-	MatrixXd weights { MatrixXd::Zero(i, j) };
-	return weights; 
-}
-
-MatrixXd train(MatrixXd oldWeights, std::vector<int> pattern, int i) {
-	VectorXd v_pattern { vec_to_vec(pattern, i) };
-	MatrixXd newWeights { v_pattern * v_pattern.transpose() - MatrixXd::Identity(i,i) };
-	return oldWeights + newWeights;
-}
-
-int getEnergy(MatrixXd weights, int i, std::vector<int> pattern) {
-	VectorXd v_pattern { vec_to_vec(pattern, weights.col(0).size()) };
-	int energy = weights.col(i - 1).dot(v_pattern.transpose()) ;
-	return energy;
-}
-
-int sign(int energy) {
-	if (energy >= 0)
-		return 1;
-	else return -1;
-}
-
-VectorXd vec_to_vec(std::vector<int> pattern, int i) {
-	VectorXd v_pattern(i);
-	for (auto x = 0; x < i; x++) {
-		v_pattern(x) = pattern[x];
-	}
-	return v_pattern;
 }
---
+++ Project1/Hopfield.h
@@ -0,0 +1,62 @@
+#pragma once
+#include <iostream>
+#include <Eigen/Dense> 
+#include <vector>
+#include <random>
+
+using namespace Eigen;
+
+MatrixXd createWeights(int, int);
+MatrixXd train(MatrixXd, std::vector<int>, int);
+double getEnergy(MatrixXd, int, VectorXd);
+VectorXd vec_to_vec(std::vector<int>, int);
+double sign(double);
+VectorXd recover(MatrixXd, VectorXd);
+
+MatrixXd createWeights(int i, int j) {
+	MatrixXd weights{ MatrixXd::Zero(i, j) };
+	return weights;
+}
+
+MatrixXd train(MatrixXd oldWeights, std::vector<int> pattern, int i) {
+	VectorXd v_pattern{ vec_to_vec(pattern, i) };
+	MatrixXd newWeights{ v_pattern * v_pattern.transpose() - MatrixXd::Identity(i,i) };
+	return oldWeights + newWeights;
+}
+
+double getEnergy(MatrixXd weights, int i, VectorXd v_pattern) {
+	double energy{ weights.col(i).dot(v_pattern.transpose()) };
+	return energy;
+}
+
+double sign(double energy) {
+	if (energy >= 0)
+		return 1.0;
+	else return -1.0;
+}
+
+VectorXd vec_to_vec(std::vector<int> pattern, int i) {
+	VectorXd v_pattern(i);
+	for (auto x = 0; x < i; x++) {
+		v_pattern(x) = pattern[x];
+	}
+	return v_pattern;
+}
+
+VectorXd recover(MatrixXd weights, std::vector<int> pattern) {
+	auto unchanged = 0;
+	VectorXd v_pattern {vec_to_vec(pattern, pattern.size()) };
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_int_distribution<int> u(0, v_pattern.size() - 1);
+
+	for (auto x = 0; x < (v_pattern.size() * 4) || unchanged >= 5; x++) {
+		int index = u(rng);
+		double activation { sign(getEnergy(weights, index, v_pattern)) };
+		if (activation == 0)
+			unchanged += 1; 
+		else unchanged = 0; 
+		v_pattern(index) =  activation;
+	}
+	return v_pattern.transpose();
+}
