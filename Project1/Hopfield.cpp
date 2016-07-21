6246019696774837e5d56d6ee6137227edf1332e
Richard Herbert <richard.alan.herbert@gmail.com>
2016-07-20 22:43:45 -0800

    rename energy function

--- Project1/Hopfield.cpp
+++ Project1/Hopfield.cpp
@@ -6,7 +6,7 @@ using namespace Eigen;
 
 MatrixXd createWeights(int, int);
 MatrixXd train(MatrixXd, std::vector<int>, int);
-int update(MatrixXd, int, std::vector<int>);
+int getEnergy(MatrixXd, int, std::vector<int>);
 VectorXd vec_to_vec(std::vector<int>, int);
 int sign(int);
 
@@ -15,7 +15,7 @@ int main() {
 	MatrixXd new_weights { train(weights, { -1, 1, 1, -1, 1}, 5) };
 	MatrixXd d_new_weights { train(new_weights,{ 1, -1, 1, -1, 1 }, 5) };
 	std::cout << d_new_weights << std::endl;
-	int energy { update(d_new_weights, 3, { 1, 1, 1, 1, 1}) };
+	int energy { getEnergy(d_new_weights, 3, { 1, 1, 1, 1, 1}) };
 	std::cout << "Energy of third node is " << energy << std::endl;
 	return 0;
 }
@@ -31,7 +31,7 @@ MatrixXd train(MatrixXd oldWeights, std::vector<int> pattern, int i) {
 	return oldWeights + newWeights;
 }
 
-int update(MatrixXd weights, int i, std::vector<int> pattern) {
+int getEnergy(MatrixXd weights, int i, std::vector<int> pattern) {
 	VectorXd v_pattern { vec_to_vec(pattern, weights.col(0).size()) };
 	int energy = weights.col(i - 1).dot(v_pattern.transpose()) ;
 	return energy;
